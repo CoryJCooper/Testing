38c6da5528111a6cec068a15d77bf283b009fde4
CoryJCooper <Admin@coryjamescooper.com>
2021-11-13 09:59:29 -0500

    whacky

--- molewhack/app.js
+++ molewhack/app.js
@@ -1,36 +1,47 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+let score = document.querySelector('#score')
 
 let result = 0
-let hitPosition
-let currentTime = 60
-let timerId = null
+let CurrentTime = timeLeft.textContent
 
-function randomSquare() {
-  squares.forEach(square => {
-    square.classList.remove('mole')
-  })
+function randomSquare(){
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
 
-  let randomSquare = squares[Math.floor(Math.random() * 9)]
-  randomSquare.classList.add('mole')
-
-  hitPosition = randomSquare.id
+    //assign id of the randomPosition for later use
+    hitPosition = randomPosition.id
 }
 
-squares.forEach(square => {
-  square.addEventListener('mousedown', () => {
-    if (square.id == hitPosition) {
-      result++
-      score.textContent = result
-      hitPosition = null
-    }
-  })
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition){
+            result = result + 1
+            score.textContent = result
+        }
+    })
 })
 
-function moveMole() {
-  timerId = setInterval(randomSquare, 500)
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+
 }
 
 moveMole()
+
+function countDown() {
+    CurrentTime--
+    timeLeft.textContent = CurrentTime
+
+    if(CurrentTime === 0){
+        clearInterval(timerId)
+        alert('Game Over! Your final score is' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
