f874173089a0cc45e6c1056dfa15a55c70d3ae86
CoryJCooper <Admin@coryjamescooper.com>
2021-11-14 10:11:31 -0500

    Had to copy and paste code over to see if mine is broken or the code is broken

--- connect4/app.js
+++ connect4/app.js
@@ -4,28 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayCurrentPlayer = document.querySelector('#current-player')
     let currentPlayer = 1
 
-    for (var i = 0, len = squares.length; i < len; i++)
-
-    (function(index)){
-        //ad an onclick to each square in your grid
-        squares[i].onclick = function(){
-            //if the square below your current square is taken
-            if(squares[index + 7].classList.contains('taken')){
-                if (currentPlayer == 1){
-                    squares[index].classList.add('taken')
-                    squares[index].classList.add('player-one')
-                    //change the player
-                    currentPlayer = 2
-                    displayCurrentPlayer.innerHTML = currentPlayer
-                } else if (currentPlayer === 2){
-                    squares[index].classList.add('taken')
-                    squares[index].classList.add('player-two')
-                    //change the player
-                    currentPlayer = 1
-                    displayCurrentPlayer.innerHTML = currentPlayer
-                }
-                //if the square below your current square is not taken, you can't go here
-            } else alert('cant go here')
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+          //if the square below your current square is taken, you can go ontop of it
+          if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-one')
+              currentPlayer = 2
+              displayCurrentPlayer.innerHTML = currentPlayer
+            } else if (currentPlayer == 2){
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-two')
+              currentPlayer = 1
+              displayCurrentPlayer.innerHTML = currentPlayer        
+            } 
+          } else alert('cant go here')
+          checkBoard()
         }
-    }
-})
+      }
+      
+    })
